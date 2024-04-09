c2f2977cd6af925dfbd79a9e6df3a62abcab705e
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2024-04-09 22:33:30 +0900

    style: apply pre-commit (#10)
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- include/mtr/cuda_helper.hpp
+++ include/mtr/cuda_helper.hpp
@@ -1,8 +1,20 @@
-/*
- * This code is licensed under CC0 1.0 Universal (Public Domain).
- * You can use this without any limitation.
- * https://creativecommons.org/publicdomain/zero/1.0/deed.en
- */
+// Copyright 2024 Kotaro Uetake
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This code is licensed under CC0 1.0 Universal (Public Domain).
+// You can use this without any limitation.
+// https://creativecommons.org/publicdomain/zero/1.0/deed.en
 
 #ifndef MTR__CUDA_HELPER_HPP_
 #define MTR__CUDA_HELPER_HPP_
