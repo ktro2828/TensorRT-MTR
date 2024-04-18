f0759943cb4da7b173d39b008bd2e3dcc8db4178
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2024-04-19 01:43:12 +0900

    feat: polyline preprocess (#14)
    * tmp: update topk kernel
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * feat: add support of extracting topk polylines
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    * feat: update polyline preprocesses
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>
    
    ---------
    
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- lib/src/preprocess/polyline_preprocess_kernel.cu
+++ lib/src/preprocess/polyline_preprocess_kernel.cu
@@ -20,8 +20,8 @@
 #include <iostream>
 
 __global__ void transformPolylineKernel(
-  const int K, const int P, const int PointDim, const float * in_polyline, const int B,
-  const int AgentDim, const float * target_state, float * out_polyline, bool * out_polyline_mask)
+  const int K, const int P, const int PointDim, const float * inPolyline, const int B,
+  const int AgentDim, const float * targetState, float * outPolyline, bool * outPolylineMask)
 {
   int b = blockIdx.x * blockDim.x + threadIdx.x;
   int k = blockIdx.y * blockDim.y + threadIdx.y;
@@ -31,52 +31,59 @@ __global__ void transformPolylineKernel(
     return;
   }
 
-  const int src_polyline_idx = (k * P + p) * PointDim;
-  const float x = in_polyline[src_polyline_idx];
-  const float y = in_polyline[src_polyline_idx + 1];
-  const float z = in_polyline[src_polyline_idx + 2];
-  const float dx = in_polyline[src_polyline_idx + 3];
-  const float dy = in_polyline[src_polyline_idx + 4];
-  const float dz = in_polyline[src_polyline_idx + 5];
-  const float type_id = in_polyline[src_polyline_idx + 6];
-
-  const int center_idx = b * AgentDim;
-  const float center_x = target_state[center_idx];
-  const float center_y = target_state[center_idx + 1];
-  const float center_z = target_state[center_idx + 2];
-  const float center_yaw = target_state[center_idx + 6];
-  const float center_cos = cos(center_yaw);
-  const float center_sin = sin(center_yaw);
-
-  // do transform
-  const float trans_x = center_cos * (x - center_x) - center_sin * (y - center_y);
-  const float trans_y = center_sin * (x - center_x) + center_cos * (y - center_y);
-  const float trans_z = z - center_z;
-  const float trans_dx = center_cos * dx - center_sin * dy;
-  const float trans_dy = center_sin * dx + center_cos * dy;
-  const float trans_dz = dz;
-
-  const int out_idx = (b * K * P + k * P + p) * (PointDim + 2);
-  out_polyline[out_idx] = trans_x;
-  out_polyline[out_idx + 1] = trans_y;
-  out_polyline[out_idx + 2] = trans_z;
-  out_polyline[out_idx + 3] = trans_dx;
-  out_polyline[out_idx + 4] = trans_dy;
-  out_polyline[out_idx + 5] = trans_dz;
-  out_polyline[out_idx + 6] = type_id;
-
-  bool is_valid = false;
-  for (size_t i = 0; i < PointDim - 1; ++i) {
-    if (out_polyline[out_idx + i] != 0.0f) {
-      is_valid = true;
+  const int inIdx = (k * P + p) * PointDim;
+  const int outIdx = b * K * P + k * P + p;
+  bool isValid = false;
+  for (int d = 0; d < PointDim - 1; ++d) {
+    if (inPolyline[inIdx + d] != 0.0f) {
+      isValid = true;
     }
   }
-  int out_mask_idx = b * K * P + k * P + p;
-  out_polyline_mask[out_mask_idx] = is_valid;
+  outPolylineMask[outIdx] = isValid;
+
+  // initialize output polyline with 0.0
+  for (int d = 0; d < PointDim + 2; ++d) {
+    outPolyline[outIdx * (PointDim + 2) + d] = 0.0f;
+  }
+
+  // set transformed values if valid, otherwise all 0.0.
+  if (isValid) {
+    const float x = inPolyline[inIdx];
+    const float y = inPolyline[inIdx + 1];
+    const float z = inPolyline[inIdx + 2];
+    const float dx = inPolyline[inIdx + 3];
+    const float dy = inPolyline[inIdx + 4];
+    const float dz = inPolyline[inIdx + 5];
+    const float typeID = inPolyline[inIdx + 6];
+
+    const int centerIdx = b * AgentDim;
+    const float centerX = targetState[centerIdx];
+    const float centerY = targetState[centerIdx + 1];
+    const float centerZ = targetState[centerIdx + 2];
+    const float centerYaw = targetState[centerIdx + 6];
+    const float centerCos = cosf(centerYaw);
+    const float centerSin = sinf(centerYaw);
+
+    // do transform
+    const float transX = centerCos * (x - centerX) - centerSin * (y - centerY);
+    const float transY = centerSin * (x - centerX) + centerCos * (y - centerY);
+    const float transZ = z - centerZ;
+    const float transDx = centerCos * dx - centerSin * dy;
+    const float transDy = centerSin * dx + centerCos * dy;
+    const float transDz = dz;
+
+    outPolyline[outIdx * (PointDim + 2)] = transX;
+    outPolyline[outIdx * (PointDim + 2) + 1] = transY;
+    outPolyline[outIdx * (PointDim + 2) + 2] = transZ;
+    outPolyline[outIdx * (PointDim + 2) + 3] = transDx;
+    outPolyline[outIdx * (PointDim + 2) + 4] = transDy;
+    outPolyline[outIdx * (PointDim + 2) + 5] = transDz;
+    outPolyline[outIdx * (PointDim + 2) + 6] = typeID;
+  }
 }
 
 __global__ void setPreviousPositionKernel(
-  const int B, const int K, const int P, const int D, const bool * mask, float * polyline)
+  const int B, const int K, const int P, const int D, float * polyline)
 {
   int b = blockIdx.x * blockDim.x + threadIdx.x;
   int k = blockIdx.y * blockDim.y + threadIdx.y;
@@ -86,27 +93,20 @@ __global__ void setPreviousPositionKernel(
     return;
   }
 
-  const int cur_idx = (b * K * P + k * P + p) * D;
-  const int pre_idx = k == 0 ? cur_idx : (b * K * P + (k - 1) * P + p) * D;
+  const int curIdx = (b * K * P + k * P + p) * D;
+  const int preIdx = p == 0 ? curIdx : (b * K * P + k * P + p - 1) * D;
 
-  polyline[cur_idx + D - 2] = polyline[pre_idx];
-  polyline[cur_idx + D - 1] = polyline[pre_idx + 1];
-
-  const int mask_idx = b * K * P + k * P + p;
-  if (!mask[mask_idx]) {
-    for (int d = 0; d < D; ++d) {
-      polyline[cur_idx + d] = 0.0f;
-    }
-  }
+  polyline[curIdx + D - 2] = polyline[preIdx];      // x
+  polyline[curIdx + D - 1] = polyline[preIdx + 1];  // y
 }
 
 __global__ void calculateCenterDistanceKernel(
-  const int B, const int L, const int P, const int AgentDim, const float * targetState,
-  const int PointDim, const float * inPolyline, const bool * inPolylineMask, float * outDistance)
+  const int B, const int K, const int P, const int D, const float * polyline,
+  const bool * polylineMask, float * distance)
 {
   int b = blockIdx.x * blockDim.x + threadIdx.x;
-  int l = blockIdx.y * blockDim.y + threadIdx.y;
-  if (b >= B || l >= L) {
+  int k = blockIdx.y * blockDim.y + threadIdx.y;
+  if (b >= B || k >= K) {
     return;
   }
 
@@ -114,19 +114,17 @@ __global__ void calculateCenterDistanceKernel(
   float sumX = 0.0f, sumY = 0.0f;
   int numValid = 0;
   for (int p = 0; p < P; ++p) {
-    int idx = b * L * P + l * P + p;
-    float x = inPolyline[idx * PointDim];
-    float y = inPolyline[idx * PointDim + 1];
-    if (inPolylineMask[idx]) {
-      sumX += x;
-      sumY += y;
+    int idx = b * K * P + k * P + p;
+    if (polylineMask[idx]) {
+      sumX += polyline[idx * D];
+      sumY += polyline[idx * D + 1];
       ++numValid;
     }
   }
   float centerX = sumX / fmaxf(1.0f, numValid);
   float centerY = sumY / fmaxf(1.0f, numValid);
 
-  outDistance[b * L + l] = sqrtf(powf(centerX, 2) + powf(centerY, 2));
+  distance[b * K + k] = hypot(centerX, centerY);
 }
 
 __global__ void extractTopKPolylineKernel(
@@ -134,11 +132,13 @@ __global__ void extractTopKPolylineKernel(
   const bool * inPolylineMask, const float * inDistance, float * outPolyline,
   bool * outPolylineMask)
 {
-  int b = blockIdx.x;  // Batch index
+  int b = blockIdx.x;                             // Batch index
+  int p = blockIdx.y * blockDim.y + threadIdx.y;  // Point index
+  int d = blockIdx.z * blockDim.z + threadIdx.z;  // Dim index
   extern __shared__ float distances[];
 
   // Load distances into shared memory
-  int tid = threadIdx.x;
+  int tid = threadIdx.x;  // Polyline index
   if (tid < L) {
     distances[tid] = inDistance[b * L + tid];
   }
@@ -156,29 +156,22 @@ __global__ void extractTopKPolylineKernel(
         minIndex = l;
       }
     }
+    __syncthreads();
 
     if (tid == k) {  // this thread will handle copying the k-th smallest polyline
-      for (int p = 0; p < P; p++) {
-        for (int d = 0; d < D; d++) {
-          outPolyline[b * K * P * D + k * P * D + p * D + d] =
-            inPolyline[b * L * P * D + minIndex * P * D + p * D + d];
-        }
-        outPolylineMask[b * K * P + k * P + p] = inPolylineMask[b * L * P + minIndex * P + p];
-      }
+      int inIdx = b * L * P + minIndex * P + p;
+      int outIdx = b * K * P + k * P + p;
+      outPolyline[outIdx * D + d] = inPolyline[inIdx * D + d];
+      outPolylineMask[outIdx] = inPolylineMask[inIdx];
     }
     distances[minIndex] = FLT_MAX;  // exclude this index from future consideration
   }
 }
 
 __global__ void calculatePolylineCenterKernel(
-  const int B, const int K, const int P, const int PointDim, const float * polyline,
-  const bool * mask, float * center)
+  const int B, const int K, const int P, const int D, const float * polyline,
+  const bool * polylineMask, float * center)
 {
-  // --- pseudo code ---
-  // sum = (polylines[:, :, :, 0:3] * mask[:, :, :, None]).sum(dim=2)
-  // center = sum / clampMIN(mask.sum(dim=2), min=1.0)
-  // -------------------
-
   int b = blockIdx.x * blockDim.x + threadIdx.x;
   int k = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -187,67 +180,70 @@ __global__ void calculatePolylineCenterKernel(
   }
 
   // initialize with 0.0
-  int center_idx = (b * K + k) * 3;
+  int centerIdx = (b * K + k) * 3;
   for (int d = 0; d < 3; ++d) {
-    center[center_idx + d] = 0.0f;
+    center[centerIdx + d] = 0.0f;
   }
 
-  float sum_xyz[3] = {0.0f, 0.0f, 0.0f};
-  int count = 0;
+  // calculate polyline center
+  float sumX = 0.0f, sumY = 0.0f, sumZ = 0.0f;
+  int numValid = 0;
   for (int p = 0; p < P; ++p) {
-    int src_idx = b * K * P + k * P + p;
-    if (mask[src_idx]) {
-      for (int d = 0; d < 3; ++d) {
-        sum_xyz[d] += polyline[src_idx * PointDim + d];
-      }
-      ++count;
+    int idx = b * K * P + k * P + p;
+    if (polylineMask[idx]) {
+      sumX += polyline[idx * D];
+      sumY += polyline[idx * D + 1];
+      sumZ += polyline[idx * D + 2];
+      ++numValid;
     }
   }
-  count = max(count, 1);
 
-  for (int d = 0; d < 3; ++d) {
-    center[center_idx + d] = sum_xyz[d] / static_cast<float>(count);
-  }
+  center[centerIdx] = sumX / fmaxf(1.0f, numValid);
+  center[centerIdx + 1] = sumY / fmaxf(1.0f, numValid);
+  center[centerIdx + 2] = sumZ / fmaxf(1.0f, numValid);
 }
 
 cudaError_t polylinePreprocessWithTopkLauncher(
-  const int L, const int K, const int P, const int PointDim, const float * in_polyline, const int B,
-  const int AgentDim, const float * target_state, float * out_polyline, bool * out_polyline_mask,
-  float * out_polyline_center, cudaStream_t stream)
+  const int K, const int L, const int P, const int PointDim, const float * inPolyline, const int B,
+  const int AgentDim, const float * targetState, float * outPolyline, bool * outPolylineMask,
+  float * outPolylineCenter, cudaStream_t stream)
 {
   if (L < K) {
     std::cerr << "L must be greater than K, but got L: " << L << ", K: " << K << std::endl;
     return cudaError_t::cudaErrorInvalidValue;
   }
 
+  const int outPointDim = PointDim + 2;
+
   float *tmpPolyline, *tmpDistance;
   bool * tmpPolylineMask;
-  CHECK_CUDA_ERROR(cudaMallocAsync(&tmpPolyline, sizeof(float) * B * L * P * PointDim, stream));
+  CHECK_CUDA_ERROR(cudaMallocAsync(&tmpPolyline, sizeof(float) * B * L * P * outPointDim, stream));
   CHECK_CUDA_ERROR(cudaMallocAsync(&tmpPolylineMask, sizeof(bool) * B * L * P, stream));
   CHECK_CUDA_ERROR(cudaMallocAsync(&tmpDistance, sizeof(float) * B * L, stream));
 
   // TODO: update the number of blocks and threads to guard from `cudaErrorIllegalAccess`
   constexpr int threadsPerBlock = 256;
-  const dim3 block3dl(B, L / threadsPerBlock, P);
-  transformPolylineKernel<<<block3dl, threadsPerBlock, 0, stream>>>(
-    L, P, PointDim, in_polyline, B, AgentDim, target_state, tmpPolyline, tmpPolylineMask);
+  const dim3 blocks1(B, L / threadsPerBlock, P);
+  transformPolylineKernel<<<blocks1, threadsPerBlock, 0, stream>>>(
+    L, P, PointDim, inPolyline, B, AgentDim, targetState, tmpPolyline, tmpPolylineMask);
 
-  const dim3 block2dl(B, L / threadsPerBlock);
-  calculateCenterDistanceKernel<<<block2dl, threadsPerBlock, 0, stream>>>(
-    B, L, P, AgentDim, target_state, PointDim, tmpPolyline, tmpPolylineMask, tmpDistance);
+  const dim3 blocks2(B, L / threadsPerBlock);
+  calculateCenterDistanceKernel<<<blocks2, threadsPerBlock, 0, stream>>>(
+    B, L, P, outPointDim, tmpPolyline, tmpPolylineMask, tmpDistance);
 
-  const size_t sharedMemSize = sizeof(float) * K;
-  extractTopKPolylineKernel<<<B, threadsPerBlock, sharedMemSize, stream>>>(
-    K, B, L, P, PointDim, tmpPolyline, tmpPolylineMask, tmpDistance, out_polyline,
-    out_polyline_mask);
+  const dim3 blocks3(B, P, outPointDim);
+  const size_t sharedMemSize = sizeof(float) * L;
+  extractTopKPolylineKernel<<<blocks3, threadsPerBlock, sharedMemSize, stream>>>(
+    K, B, L, P, outPointDim, tmpPolyline, tmpPolylineMask, tmpDistance, outPolyline,
+    outPolylineMask);
 
-  const dim3 block3dk(B, K / threadsPerBlock, P);
-  setPreviousPositionKernel<<<block3dk, threadsPerBlock, 0, stream>>>(
-    B, K, P, PointDim, out_polyline_mask, out_polyline);
+  const dim3 blocks4(B, K / threadsPerBlock, P);
+  setPreviousPositionKernel<<<blocks4, threadsPerBlock, 0, stream>>>(
+    B, K, P, outPointDim, outPolyline);
 
-  const dim3 block2dk(B, K / threadsPerBlock);
-  calculatePolylineCenterKernel<<<block2dk, threadsPerBlock, 0, stream>>>(
-    B, K, P, PointDim, out_polyline, out_polyline_mask, out_polyline_center);
+  const dim3 blocks5(B, K / threadsPerBlock);
+  calculatePolylineCenterKernel<<<blocks5, threadsPerBlock, 0, stream>>>(
+    B, K, P, outPointDim, outPolyline, outPolylineMask, outPolylineCenter);
 
   CHECK_CUDA_ERROR(cudaFree(tmpPolyline));
   CHECK_CUDA_ERROR(cudaFree(tmpPolylineMask));
@@ -257,23 +253,25 @@ cudaError_t polylinePreprocessWithTopkLauncher(
 }
 
 cudaError_t polylinePreprocessLauncher(
-  const int K, const int P, const int PointDim, const float * in_polyline, const int B,
-  const int AgentDim, const float * target_state, float * out_polyline, bool * out_polyline_mask,
-  float * out_polyline_center, cudaStream_t stream)
+  const int K, const int P, const int PointDim, const float * inPolyline, const int B,
+  const int AgentDim, const float * targetState, float * outPolyline, bool * outPolylineMask,
+  float * outPolylineCenter, cudaStream_t stream)
 {
+  const int outPointDim = PointDim + 2;
+
   // TODO: update the number of blocks and threads to guard from `cudaErrorIllegalAccess`
   constexpr int threadsPerBlock = 256;
   const dim3 block3d(B, (K - 1) / threadsPerBlock, P);
 
   transformPolylineKernel<<<block3d, threadsPerBlock, 0, stream>>>(
-    K, P, PointDim, in_polyline, B, AgentDim, target_state, out_polyline, out_polyline_mask);
+    K, P, PointDim, inPolyline, B, AgentDim, targetState, outPolyline, outPolylineMask);
 
   setPreviousPositionKernel<<<block3d, threadsPerBlock, 0, stream>>>(
-    B, K, P, PointDim, out_polyline_mask, out_polyline);
+    B, K, P, outPointDim, outPolyline);
 
   const dim3 block2d(B, (K - 1) / threadsPerBlock);
   calculatePolylineCenterKernel<<<block2d, threadsPerBlock, 0, stream>>>(
-    B, K, P, PointDim, out_polyline, out_polyline_mask, out_polyline_center);
+    B, K, P, outPointDim, outPolyline, outPolylineMask, outPolylineCenter);
 
   return cudaGetLastError();
 }
