7aac2a1ddb020b5272f84b2abc66a2f9580e1f23
ktro2828 <kotaro.uetake@tier4.jp>
2024-04-23 13:58:24 +0900

    feat: load input date from dat files
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- src/main.cpp
+++ src/main.cpp
@@ -12,96 +12,120 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "mtr/agent.hpp"
 #include "mtr/debugger.hpp"
 #include "mtr/mtr.hpp"
-
-#include <math.h>
+#include "mtr/polyline.hpp"
+#include "mtr/trajectory.hpp"
 
 #include <cassert>
+#include <fstream>
+#include <map>
 #include <string>
+#include <vector>
 
 static const std::string USAGE =
   "[USAGE]: ./build/main <PATH_TO_ONNX>(or <PATH_TO_ENGINE>) [<NUM_REPEAT=1>]";
 
-mtr::AgentData load_agent_data()
+/**
+ * @brief Load timestamps from dat file.
+ *
+ * @param filepath
+ * @return std::vector<float>
+ */
+std::vector<float> loadTimestamp(const std::string & filepath)
 {
-  // NOTE: current expected input size is (3, 55, 11, 12)
-  constexpr int B = 3;
-  constexpr int N = 55;
-  constexpr int T = 11;
-  constexpr int D = 12;
-  constexpr int sdc_index = 1;
-
-  std::vector<float> timestamps{0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
-
-  std::vector<int> target_index;
-  for (int n = 0; n < N; ++n) {
-    if (n != sdc_index) {
-      target_index.emplace_back(n);
-    }
-    if (target_index.size() == B) {
-      break;
-    }
+  std::ifstream file(filepath);
+  if (!file.is_open()) {
+    std::ostringstream err_msg;
+    err_msg << "Failed to open timestamp file: " << filepath;
+    throw std::runtime_error(err_msg.str());
   }
 
-  std::vector<int> label_index;
-  float trajectory[N][T][D];
-  for (int n = 0; n < N; ++n) {
-    label_index.emplace_back(0);
-    for (int t = 0; t < T; ++t) {
-      for (int d = 0; d < D; ++d) {
-        trajectory[n][t][d] = static_cast<float>(n * t + d);
-      }
-    }
+  std::vector<float> timestamps;
+  while (!file.eof()) {
+    float t;
+    file >> t;
+    timestamps.emplace_back(t);
   }
+  file.close();
+  return timestamps;
+}
+
+/**
+ * @brief Load `mtr::AgentData` from dat files.
+ *
+ * @return mtr::AgentData
+ */
+mtr::AgentData loadAgentData()
+{
+  const std::vector<float> timestamps = loadTimestamp("./data/Timestamp.dat");
+  const auto max_time_length = timestamps.size();
+
+  // [agent_id, filepath]: (B, N) = (2, 7)
+  const std::map<std::string, std::string> filepaths = {
+    {"Target0", "./data/TargetVehicle0.dat"},
+    {"Target1", "./data/TargetVehicle1.dat"},
+    {"Agent0", "./data/AgentVehicle0.dat"},
+    {"Agent1", "./data/AgentVehicle1.dat"},
+    {"Agent2", "./data/AgentVehicle2.dat"},
+    {"Agent3", "./data/AgentVehicle3.dat"},
+    {"EGO", "./data/Ego.dat"}};
+
+  const std::vector<int> target_index = {0, 1};
+  const std::vector<int> label_index = {0, 0, 0, 0, 0, 0, 0};
+  constexpr int sdc_index = 6;
 
   std::vector<mtr::AgentHistory> histories;
-  histories.reserve(N);
-  for (size_t n = 0; n < N; ++n) {
-    mtr::AgentHistory history("foo", T);
-    for (size_t t = 0; t < T; ++t) {
-      const size_t idx = n * T * D + t * D;
-      auto x = trajectory[n][t][0];
-      auto y = trajectory[n][t][1];
-      auto z = trajectory[n][t][2];
-      auto length = trajectory[n][t][3];
-      auto width = trajectory[n][t][4];
-      auto height = trajectory[n][t][5];
-      auto yaw = trajectory[n][t][6];
-      auto vx = trajectory[n][t][7];
-      auto vy = trajectory[n][t][8];
-      auto ax = trajectory[n][t][9];
-      auto ay = trajectory[n][t][10];
-      auto is_valid = trajectory[n][t][11];
-      mtr::AgentState state(x, y, z, length, width, height, yaw, vx, vy, ax, ay, is_valid);
-      history.update(static_cast<float>(t), state);
+  for (const auto & [agent_id, filepath] : filepaths) {
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+      std::ostringstream err_msg;
+      err_msg << "Failed to open agent file: " << filepath;
+      throw std::runtime_error(err_msg.str());
     }
+
+    mtr::AgentHistory history(agent_id, max_time_length);
+    size_t cnt = 0;
+    while (!file.eof()) {
+      auto current_time = timestamps.at(cnt);
+
+      float x, y, z, length, width, height, yaw, vx, vy, ax, ay, is_valid;
+      file >> x >> y >> z >> length >> width >> height >> yaw >> vx >> vy >> ax >> ay >> is_valid;
+      mtr::AgentState state(x, y, z, length, width, height, yaw, vx, vy, ax, ay, is_valid);
+      history.update(current_time, state);
+      ++cnt;
+    }
+    file.close();
     histories.emplace_back(history);
   }
   return mtr::AgentData(histories, sdc_index, target_index, label_index, timestamps);
 }
 
-mtr::PolylineData load_polyline_data(const int K)
+/**
+ * @brief Load `mtr::PolylineData` from dat file.
+ *
+ * @return mtr::PolylineData
+ */
+mtr::PolylineData loadPolylineData(const size_t K, const size_t P, const float threshold)
 {
-  constexpr int N = 2500;
-  constexpr int P = 20;
-  constexpr int D = 7;
+  const std::string filepath = "./data/Polyline.dat";
 
-  std::vector<mtr::LanePoint> points;
-  points.reserve(N);
-  for (std::size_t n = 0; n < N; ++n) {
-    float x = 1.0f;
-    float y = 1.0f;
-    auto z = 1.0f;
-    auto dx = 1.0f;
-    auto dy = 1.0f;
-    auto dz = 1.0f;
-    auto label = 1.0f;
-    mtr::LanePoint point(x, y, z, dx, dy, dz, label);
-    points.emplace_back(point);
+  std::ifstream file(filepath);
+  if (!file.is_open()) {
+    std::ostringstream err_msg;
+    err_msg << "Failed to open polyline file: " << filepath;
+    throw std::runtime_error(err_msg.str());
   }
 
-  return mtr::PolylineData(points, K, P, 2.0f);
+  std::vector<mtr::LanePoint> points;
+  while (!file.eof()) {
+    float x, y, z, dx, dy, dz, label;
+    file >> x >> y >> z >> dx >> dy >> dz >> label;
+    points.emplace_back(x, y, z, dx, dy, dz, label);
+  }
+  file.close();
+  return mtr::PolylineData(points, K, P, threshold);
 }
 
 int main(int argc, char ** argv)
@@ -115,8 +139,10 @@ int main(int argc, char ** argv)
   auto model = std::make_unique<mtr::TrtMTR>(model_path, "FP32");
 
   debugger.createEvent();
-  auto agent_data = load_agent_data();
-  auto polyline_data = load_polyline_data(model->config().max_num_polyline);
+  auto config = model->config();
+  auto agent_data = loadAgentData();
+  auto polyline_data =
+    loadPolylineData(config.max_num_polyline, config.max_num_point, config.point_break_distance);
   debugger.printElapsedTime("Data loading time: ");
 
   for (int i = 0; i < num_repeat; ++i) {
